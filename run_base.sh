21883df13b4ba06997eee9b4e7b33547a81cee9e
yuleiqin <yuleiqin@tencent.com>
2025-06-22 14:14:04 +0800

    remove sensitive info

--- run_base.sh
+++ run_base.sh
@@ -1,17 +1,5 @@
-export http_proxy="http://172.31.255.10:8888"
-export https_proxy="http://172.31.255.10:8888"
-export no_proxy=".woa.com,mirrors.cloud.tencent.com,tlinux-mirror.tencent-cloud.com,tlinux-mirrorlist.tencent-cloud.com,localhost,127.0.0.1,mirrors-tlinux.tencentyun.com,.oa.com,.local,.3gqq.com,.7700.org,.ad.com,.ada_sixjoy.com,.addev.com,.app.local,.apps.local,.aurora.com,.autotest123.com,.bocaiwawa.com,.boss.com,.cdc.com,.cdn.com,.cds.com,.cf.com,.cjgc.local,.cm.com,.code.com,.datamine.com,.dvas.com,.dyndns.tv,.ecc.com,.expochart.cn,.expovideo.cn,.fms.com,.great.com,.hadoop.sec,.heme.com,.home.com,.hotbar.com,.ibg.com,.ied.com,.ieg.local,.ierd.com,.imd.com,.imoss.com,.isd.com,.isoso.com,.itil.com,.kao5.com,.kf.com,.kitty.com,.lpptp.com,.m.com,.matrix.cloud,.matrix.net,.mickey.com,.mig.local,.mqq.com,.oiweb.com,.okbuy.isddev.com,.oss.com,.otaworld.com,.paipaioa.com,.qqbrowser.local,.qqinternal.com,.qqwork.com,.rtpre.com,.sc.oa.com,.sec.com,.server.com,.service.com,.sjkxinternal.com,.sllwrnm5.cn,.sng.local,.soc.com,.t.km,.tcna.com,.teg.local,.tencentvoip.com,.tenpayoa.com,.test.air.tenpay.com,.tr.com,.tr_autotest123.com,.vpn.com,.wb.local,.webdev.com,.webdev2.com,.wizard.com,.wqq.com,.wsd.com,.sng.com,.music.lan,.mnet2.com,.tencentb2.com,.tmeoa.com,.pcg.com,www.wip3.adobe.com,www-mm.wip3.adobe.com,mirrors.tencent.com,csighub.tencentyun.com,mirrors.tencentyun.com,mirrors.tencent.com"
-
-# export NCCL_SOCKET_IFNAME=eth0
-# export NCCL_IB_DISABLE=0
-# export NCCL_IB_GID_INDEX=3
-# export NCCL_P2P_DISABLE=0
-# export NCCL_IB_CUDA_SUPPORT=1
 
 echo $PWD
-
-# echo "INSTALL THE PACKAGE"
-# pip3 install -v -e .
 NODE_LIST=${NODE_LIST}
 GPUS_PER_NODE=${GPU_NUM_PER_NODE}
 NNODES=${NODE_NUM}
